e77b25985a9c18dbdc9cbe75ef5ceac71dd8fe1f
jonathan pabel saldivar <saldivar.jonathan@hotmail.com>
2019-10-10 10:10:33 -0500

    init

---
+++ 01-MiPrimeraApp/01-MiPrimeraApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  01-MiPrimeraApp
+//
+//  Created by Jonathan Pabel Saldivar Mendoza on 10/10/19.
+//  Copyright © 2019 moobile. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
